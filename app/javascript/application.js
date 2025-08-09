5833ebf9d306491249ce1c22af121bd22d87bda9
Clint Ryan <clintryan@Clints-Mac-mini.local>
2025-08-09 10:04:03 +0700

    Update: Homepage styling and contact page refinements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "@fortawesome/fontawesome-free" 
 
 // Enhanced section transitions
 document.addEventListener('DOMContentLoaded', function() {
